024003193d31451b1680266d5ffff89977143214
Ibrahim AbdElwahab <iabdwahab20@gmail.com>
2024-05-07 04:24:39 +0300

    Add some sections to "Bookmark Landing Page"

--- solutions/bookmark-landing-page/js/script.js
+++ solutions/bookmark-landing-page/js/script.js
@@ -1,3 +1,4 @@
+// Start Header Nav Menu
 const headerEl = document.querySelector('.header');
 const headerMenuBtn = document.querySelector('.header__nav-menu-btn');
 const headerNavCloseBtn = document.querySelector('.header__nav-menu-top-close-btn');
@@ -8,4 +9,25 @@ headerMenuBtn.addEventListener('click', () => {
 
 headerNavCloseBtn.addEventListener('click', () => {
   headerEl.classList.remove('header__menu-nav--open');
+})
+// End Header Nav Menu
+
+
+// Features Section Selector
+const featuresSelectorBtns = document.querySelectorAll('.features-section__selector-btn');
+
+featuresSelectorBtns.forEach((btn) => {
+
+  btn.addEventListener('click', () => {
+    const featureName = btn.dataset.featureName;
+
+    document.querySelector('.features__feature--active').classList.remove('features__feature--active');
+    document.querySelector(`.features__feature-${featureName}`).classList.add('features__feature--active');
+
+
+    document.querySelector('.features-section__selector-btn--active').classList.remove('features-section__selector-btn--active');
+    btn.classList.add('features-section__selector-btn--active');
+
+  });
+
 })
